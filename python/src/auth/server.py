b67a1c4a442a14fdf160279f43de21284d10fbc5
nicholasg2001 <nicholasgutierrez62001@gmail.com>
2024-07-11 13:37:45 -0400

    initial commit

---
+++ python/src/auth/server.py
@@ -0,0 +1,17 @@
+import jwt, datetime, os
+from flask import Flask, request
+from flask_mysqldb import MySQL
+
+server = Flask(__name__) #app
+mysql = MySQL(server)
+
+#config for server/app
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST") #resolves to localhost
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+
+@server.route("/login", methods=["POST"])
+def login():
+    auth = request.authorization
